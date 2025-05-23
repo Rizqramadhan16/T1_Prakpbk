1dce456f26bfae035546516974c8494928e05510
Rizqiramadhan16 <rizqimadan1610@gmail.com>
2025-05-23 21:21:45 +0700

    penambahan bio data diri

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,10 +13,11 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <p>Nama : Rizqi Ramadhan</p>
+    <p>NPM : 233510156</p>
+    <p>Umur : 21</p>
+    <p>Hobi : Turu</p>
+    <p>Alamat : Duta Mas</p>  
   </div>
 
   <p>
